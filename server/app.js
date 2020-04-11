1d081be65d23077585b357924295a8fc3eb468b4
sterlu <sterlu.sd@gmail.com>
2020-04-11 21:25:24 +0200

    SSL

--- server/app.js
+++ server/app.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const path = require('path');
+const fs = require('fs');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const http = require('http');
+const https = require('https');
 const io = require('socket.io');
 
 const createGame = require('./src/game');
+
+const isProd = process.env.NODE_ENV === 'production';
 
 const app = express();
 
@@ -16,13 +20,31 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, '../client/build/')));
 app.get('*', (req, res) => res.redirect('/'));
 
-const port = 6600;
-const server = http.createServer(app);
-server.listen(port, () => console.log(`App listening on port ${port}`));
+app.use((req, res, next) => {
+  (isProd && !req.secure)
+    ? res.redirect('https://' + req.headers.host + req.url)
+    : next();
+});
+
+const httpServer = http.createServer(app);
+httpServer.listen(80);
+
+const key = isProd
+  ? fs.readFileSync('/etc/letsencrypt/live/greenpeaks.online/privkey.pem', 'utf8')
+  : fs.readFileSync(path.resolve('./ssl/localhost.key'), 'utf8');
+const cert = isProd
+  ? fs.readFileSync('/etc/letsencrypt/live/greenpeaks.online/cert.pem', 'utf8')
+  : fs.readFileSync(path.resolve('./ssl/localhost.crt'), 'utf8');
+const ca = isProd
+  ? fs.readFileSync('/etc/letsencrypt/live/greenpeaks.online/chain.pem', 'utf8')
+  : undefined;
+
+const httpsServer = https.createServer({ key, cert, ca }, app);
+httpsServer.listen(443);
 
 const game = createGame();
 
-const socketServer = io(server);
+const socketServer = io(httpsServer);
 socketServer.on('connection', function (socket) {
   socket.on('disconnect', function () {
     game.removePlayer(socket.id);
